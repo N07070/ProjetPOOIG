182d6ea3775cfe005479c3ec88a5838d4c8be907
Thomas Copt-Bignon <lowx.add@gmail.com>
2018-11-08 15:26:24 +0100

    Création des classes

---
+++ DominoGommette.java
@@ -0,0 +1,6 @@
+public class DominoGommette extends Domino {
+  @Override
+  public void afficher(){
+    // Remplacer les chiffres par des couleurs
+  }
+}
---
+++ Lanceur.java
@@ -0,0 +1,5 @@
+public class Lanceur {
+  public static void main(String[] args) {
+    
+  }
+}
